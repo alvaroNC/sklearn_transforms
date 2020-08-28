df4c012ea53dd4b36cd7a9952a79fa1a54d8327f
alvaroNC <69848186+alvaroNC@users.noreply.github.com>
2020-08-28 15:51:48 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -3,7 +3,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.impute import SimpleImputer
 
 # All sklearn Transforms must have the `transform` and `fit` methods
-class DropColumns2(BaseEstimator, TransformerMixin):
+class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
         self.columns = columns
 
@@ -15,3 +15,21 @@ class DropColumns2(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+class ImputerValuesDesafio4(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.si0= 0
+        self.si1= 0
+        self.sif= 0
+        self.y_label_0=[]
+        
+    def fit(self, X, y=None):
+        self.si0.fit(X=X[self.y_label_0=='Aceptado'])
+        self.si1.fit(X=X[self.y_label_0=='Sospechoso'])
+        self.sif.fit(X=X[self.y_label_0=='Aceptado']) #doesnt matter whicch you choose.It's only to get the number of features.
+        self.sif.statistics_=(self.si0.statistics_+self.si1.statistics_)/2
+        return self
+
+    
+    def transform(self, X):
+        data=self.sif.transform(X.copy())
+        return data
